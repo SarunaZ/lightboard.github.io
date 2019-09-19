1c03aabedc4bdbcc77080c88f6bc8eaf56952f54
Sarunas Zube <sarunas.zube@gmail.com>
2019-09-19 20:39:55 +0300

    NFQ-1 Started working on the specialist page

--- src/js/manageData.js
+++ src/js/manageData.js
@@ -1,3 +1,18 @@
 import { storageData } from './storageData.js';
+import createNode from './createNode.js';
 
-console.log(storageData)
+const specialist = document.querySelector('.specialist__select');
+console.log(storageData);
+
+const rederData = () => {
+  storageData.forEach(item => {
+    const option = createNode("option", 'specialist__option');
+    specialist.appendChild(option);
+    const optionMarkup = `
+  <option value="Lol"> ${item.name} </option>
+`;
+    option.innerHTML = optionMarkup;
+  });
+}
+
+localStorage.length !== 0 ? rederData() : lightboard.innerText = "No data";
