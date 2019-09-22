fceed490e8999b180707ed071e0212c30ed582af
Sarunas Zube <sarunas.zube@gmail.com>
2019-09-22 19:46:55 +0300

    NFQ-1 completed first level task funcionality and styling

--- src/js/getData.js
+++ src/js/getData.js
@@ -2,28 +2,43 @@
  * @file
  */
 
-import { storageData } from './storageData.js';
+import {
+  storageData
+} from './storageData.js';
 import createNode from './createNode.js';
-
 const lightboard = document.querySelector('.lightboard');
+let current = '';
+let collumn = '';
 
 const renderData = () => {
-  let speciaList = []
- storageData.filter(item => {
-   !item.specialist.includes(speciaList) ? specialist.push(item.specialist) : '';
-  });
- console.log(speciaList);
-  storageData.forEach(item => {
-    const row = createNode("div", 'lightboard__row');
-    const markup = `
+  let sorted = storageData.sort((a, b) => {
+    if (a.specialist > b.specialist) {
+      return 1
+    } else if (a.specialist < b.specialist) {
+      return -1
+    } else {
+      if (a.number > b.number) {
+        return 1
+      } else {
+        return -1
+      }
+    }
+  })
+
+  sorted.forEach(item => {
+    if (item.specialist !== current) {
+      collumn = createNode("div", "lightboard__data");
+      lightboard ? lightboard.appendChild(collumn) : '';
+      current = item.specialist;
+      const markup = `
       <p class="lightboard__number">${item.number}</p>
       <p class="lightboard__name">Vardas: ${item.name}</p>
       <p class="lightboard__specialist">Specialistas: ${item.specialist}</p>
     `;
-    lightboard ? lightboard.appendChild(row) : '';
-    row.innerHTML = markup
+      collumn.innerHTML = markup
+    }
   });
 }
 
 localStorage.length !== 0 && lightboard ? renderData() :
- lightboard ? lightboard.innerText = "No data" : '';
+  lightboard ? lightboard.innerText = "No data" : '';
