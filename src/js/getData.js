ee453d9edae8bec9abfc4219753f6e5d98f71584
Sarunas Zube <sarunas.zube@gmail.com>
2019-09-23 20:23:42 +0300

    NFQ-1 finished work

--- src/js/getData.js
+++ src/js/getData.js
@@ -1,5 +1,5 @@
 /**
- * @file
+ * @file Functionality for data rendering on the index.html page
  */
 
 import {
@@ -11,6 +11,7 @@ let current = '';
 let collumn = '';
 
 const renderData = () => {
+  // Sorting data by specialist and queue number
   let sorted = storageData.sort((a, b) => {
     if (a.specialist > b.specialist) {
       return 1
@@ -25,7 +26,9 @@ const renderData = () => {
     }
   })
 
+  // Rendering data from the sorted list
   sorted.forEach(item => {
+    // Adding one of each specialist clients to current
     if (item.specialist !== current) {
       collumn = createNode("div", "lightboard__data");
       lightboard ? lightboard.appendChild(collumn) : '';
@@ -40,5 +43,6 @@ const renderData = () => {
   });
 }
 
+// Only render data if there is data and if there is a specific element
 localStorage.length !== 0 && lightboard ? renderData() :
-  lightboard ? lightboard.innerText = "No data" : '';
+  lightboard ? lightboard.innerText = "Duomenys nėra užkrauti" : '';
